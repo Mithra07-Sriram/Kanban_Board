20755d0a1c1446fcbd9a67b19d7674a4f9f0dd8e
mithra <mithrasriram07@gmail.com>
2025-01-05 12:47:33 +0530

    Adding mangodb connection backend

--- Kanban_Board_Backend/index.js
+++ Kanban_Board_Backend/index.js
@@ -1,6 +1,17 @@
 var express = require('express')
+var path=require("path");
+var mdb=require("mongoose");
+var User=require("./models/users")
 var app = express()
 const PORT = 3002
+app.use(express.json())
+mdb.connect("mongodb://localhost:27017/").then(()=>
+ {
+    console.log("Mongodb connection Successful");
+})
+.catch(()=>{
+    console.log("Check your connection string");
+})
 app.get('/',(req,res)=>{
     res.send("Welcome to Backend Server")
 })
@@ -8,8 +19,38 @@ app.get('/json',(req,res)=>{
     res.json({server:"Welcome to Backend",url:"localhost",port:PORT})
 })
 app.get('/static',(req,res)=>{
-    res.sendFile('/Users/admin/OneDrive/Desktop/Kanban_Board/Kanban_Board_Backend/index.html')
+   // res.sendFile('/Users/admin/OneDrive/Desktop/Kanban_Board/Kanban_Board_Backend/index.html')
     //copy path change the \ to /
+    res.sendFile(path.join(__dirname,'index.html'));
+})
+app.post('/signup',(req,res)=>{
+    var{firstname,lastname,email}=req.body
+    try{
+        var user=new User({
+            firstname:firstname,
+            lastname:lastname,
+            email:email
+        })
+        user.save()
+        console.log("User Added Successfully");
+        res.status(200).send("User Added Successfully")
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+})
+app.get('/getSignup',async(req,res)=>{
+    try{
+        var allrecords=await User.find()
+        res.json(allrecords)
+        console.log("All records fetched");
+    }
+    catch(err)
+    {
+        console.log(err)
+        res.send(err)
+    }
 })
 app.listen(PORT,()=>{
     console.log(`Backend Server Started\nUrl: http://localhost:${PORT}`);
